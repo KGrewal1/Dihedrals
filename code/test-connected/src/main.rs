b951eea1db3bd66d665fa0e00d9ef89b461dbc4d
Kirpal Grewal <kirpal@outlook.com>
2024-03-17 18:03:11 +0000

    parralelise inference

--- code/test-connected/src/main.rs
+++ code/test-connected/src/main.rs
@@ -1,15 +1,15 @@
 use anyhow::anyhow;
 use candle_core::Tensor;
 use env_logger::Builder;
-use log::{info, LevelFilter};
+use log::{info, warn, LevelFilter};
 use parse_dihedrals::{Dihedral, Dihedrals};
 use parse_tsdata::TransitionStates;
+use rayon::prelude::*;
 use std::{collections::BTreeMap, fs, path::Path};
 
 mod nn_arch;
 mod setup_model;
 const NDIHEDRALS: usize = 178;
-const NCX: usize = 300_000;
 
 fn main() -> anyhow::Result<()> {
     let mut builder = Builder::new();
@@ -34,49 +34,59 @@ fn main() -> anyhow::Result<()> {
             .map_err(|err| anyhow!(err))?
             .into();
 
-    // let raw = Vec::from_raw_parts();
-    let mut count = 0;
-    let mut count_cx = 0;
-    let mut count_cx_miss = 0;
-    let mut count_ucx = 0;
-    let mut count_ucx_miss = 0;
-    'outer: for (i, min_1) in minima.iter().enumerate() {
-        for min_2 in minima.iter().skip(i + 1) {
-            let cx = connections.get(&(min_1.id, min_2.id));
-            let mut angles: Vec<f64> = Vec::with_capacity(2 * NDIHEDRALS);
-            angles.extend(min_1.dihedrals.iter());
-            angles.extend(min_2.dihedrals.iter());
-            let test_input = Tensor::from_slice(&angles, (1, 1, 2, NDIHEDRALS), &dev)?
-                .to_dtype(candle_core::DType::F32)?;
-            let pred = model.forward(&test_input)?;
-            let pred = pred.reshape(())?;
-            let pred: f32 = pred.to_scalar()?;
-            if cx.is_none() {
-                count_ucx += 1;
-
-                if pred > 0.5 {
-                    count_ucx_miss += 1;
-                }
-                if pred > 0.9999 {
-                    info! {"prob {}", pred};
-                    println!("{} {}", min_1.id, min_2.id);
-                    count += 1;
-                    if count > NCX {
-                        break 'outer;
+    let (count_cx, count_ucx, count_cx_miss, count_ucx_miss) = minima
+        .par_iter()
+        .enumerate()
+        .flat_map(|(i, min_1)| {
+            minima.par_iter().skip(i + 1).map(|min_2| {
+                let mut angles: Vec<f64> = Vec::with_capacity(2 * NDIHEDRALS);
+                angles.extend(min_1.dihedrals.iter());
+                angles.extend(min_2.dihedrals.iter());
+                let pred = Tensor::from_slice(&angles, (1, 1, 2, NDIHEDRALS), &dev)
+                    .and_then(|tensor| tensor.to_dtype(candle_core::DType::F32))
+                    .and_then(|input| model.forward(&input))
+                    .and_then(|pred| pred.reshape(()))
+                    .and_then(|pred| pred.to_scalar::<f32>());
+                (min_1.id, min_2.id, pred)
+            })
+        })
+        .map(|(min_1, min_2, pred)| {
+            if let Ok(pred) = pred {
+                let cx = connections.get(&(min_1, min_2));
+                if cx.is_none() {
+                    if pred > 0.5 {
+                        if pred > 0.9999 {
+                            info!("prob {}", pred);
+                            println!("{} {}", min_1, min_2);
+                        }
+                        (0, 1, 0, 1)
+                    } else {
+                        (0, 1, 0, 0)
+                    }
+                } else {
+                    if pred < 0.5 {
+                        (1, 0, 1, 0)
+                    } else {
+                        (1, 0, 0, 0)
                     }
                 }
             } else {
-                count_cx += 1;
-                if pred < 0.5 {
-                    count_cx_miss += 1;
-                }
+                warn!("failed prediction for {} {} {:?}", min_1, min_2, pred);
+                (0, 0, 0, 0)
             }
-        }
-        // println!("{}", i);
-        // if i == 200 {
-        //     break;
-        // }
-    }
+        })
+        .reduce(
+            || (0, 0, 0, 0),
+            |(count_cx, count_ucx, count_ucx_miss, count_cx_miss), (cx, ucx, ucx_miss, cx_miss)| {
+                (
+                    count_cx + cx,
+                    count_ucx + ucx,
+                    count_ucx_miss + ucx_miss,
+                    count_cx_miss + cx_miss,
+                )
+            },
+        );
+
     info!(
         "count_cx: {}, count_ucx: {}, count_ucx_miss: {}, count_cx_miss: {}",
         count_cx, count_ucx, count_ucx_miss, count_cx_miss
